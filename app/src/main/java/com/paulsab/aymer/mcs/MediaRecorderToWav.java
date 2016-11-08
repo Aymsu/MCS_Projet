14864bc678b83402c01e8fac5a67207024d41c37
benjidu11 <benjamingrilleres@yahoo.fr>
2016-11-08 08:44:14 +0100

    Ajout enregistrement vocal

---
+++ app/src/main/java/com/paulsab/aymer/mcs/MediaRecorderToWav.java
@@ -0,0 +1,53 @@
+package com.paulsab.aymer.mcs;
+
+
+import android.media.MediaRecorder;
+import android.os.Environment;
+
+
+import java.io.IOException;
+
+/**
+ * Created by benjidu11 on 08/11/2016.
+ */
+
+public class MediaRecorderToWav {
+
+    private MediaRecorder recorder = null;
+
+    private String filenameDirectory = new String();
+    /**
+     * Méthode exécutée quand l'utilisateur appuie sur le push-to-tal
+     */
+    public void startRecording () {
+        recorder = new MediaRecorder();
+
+        filenameDirectory = Environment.getExternalStorageDirectory().getAbsolutePath();
+        filenameDirectory += "/temp.wav";
+
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
+        recorder.setOutputFile(filenameDirectory);
+
+        try {
+            recorder.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        recorder.start();
+    }
+
+    public void stopRecording () {
+        if ( recorder != null ) {
+            recorder.stop();
+        }
+        recorder.release();
+        recorder = null;
+    }
+
+    public String getFilenameDirectory(){
+        return filenameDirectory;
+    }
+
+}
---
+++ app/src/main/java/com/paulsab/aymer/mcs/RecoVocale.java
@@ -0,0 +1,63 @@
+package com.paulsab.aymer.mcs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import java.io.IOException;
+import java.util.jar.Manifest;
+
+    public class RecoVocale extends AppCompatActivity {
+
+        // Used to load the 'native-lib' library on application startup.
+        static {
+            System.loadLibrary("dtw-lib");
+        }
+        private MediaRecorderToWav recorder = new MediaRecorderToWav();
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_reco_vocale);
+
+            final Button talkButton = (Button) findViewById(R.id.talkButton);
+
+            // Demander à l'utilisateur d'utiliser le microphone
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,new String[]{android.Manifest.permission.RECORD_AUDIO},0);
+            }
+
+            // Demander à l'utilisateur de créer des fichiers
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                String[] rules = new String[]{
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                };
+                ActivityCompat.requestPermissions(this,rules,1);
+            }
+
+            talkButton.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                        recorder.startRecording();
+                        talkButton.setText("pressssed");
+                    }
+                    else if (event.getAction() == MotionEvent.ACTION_UP){
+                        recorder.stopRecording();
+                        talkButton.setText("Released");
+                    }
+                    return false;
+                }
+            });
+
+        }
+
+    }
