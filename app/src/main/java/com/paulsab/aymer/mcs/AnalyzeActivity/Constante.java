ec1f4828981fc2bcbc1b05b71d137033653953f4
benjidu11 <benjamingrilleres@yahoo.fr>
2016-11-15 14:57:39 +0100

    ajout fonctionnalité signal

--- app/src/main/java/com/paulsab/aymer/mcs/AnalyzeActivity/Constante.java
+++ app/src/main/java/com/paulsab/aymer/mcs/AnalyzeActivity/Constante.java
@@ -11,7 +11,7 @@ public class Constante {
     public final static float MEAN_MAX = 16384f;   // Maximum signal value
     public final static int AGC_OFF = MediaRecorder.AudioSource.VOICE_RECOGNITION;
     public final static int FFT_BINS = 1024;
-    public final static int SAMPLE_RATE = 22050;
+    public final static int SAMPLE_RATE = 44100;
     public final static int UPDATE_MS = 150;
     public final static int MAX_X = 20000;
     public final static int MAX_Y = -90;
