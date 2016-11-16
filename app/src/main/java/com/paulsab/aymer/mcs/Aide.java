24de217ff5faff2f99d968aca94ea4bc8f7ed80a
Aymsu <aymeri.piganiol@live.fr>
2016-11-16 17:18:47 +0100

    ajout interface

--- app/src/main/java/com/paulsab/aymer/mcs/Aide.java
+++ app/src/main/java/com/paulsab/aymer/mcs/Aide.java
@@ -1,14 +1,21 @@
 package com.paulsab.aymer.mcs;
 
+import android.animation.Animator;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.ViewTreeObserver;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 
 public class Aide extends AppCompatActivity {
 
+    RelativeLayout rootLayoutAide;
     ArrayList<String> listcommandes = new ArrayList<String>();
 
     @Override
@@ -16,7 +23,33 @@ public class Aide extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aide);
 
+        rootLayoutAide = (RelativeLayout) findViewById(R.id.activity_aide);
+
+        ViewTreeObserver viewTreeObserver = rootLayoutAide.getViewTreeObserver();
+
+        if(savedInstanceState == null)
+            rootLayoutAide.setVisibility(View.INVISIBLE);
+
+        if(viewTreeObserver.isAlive()) {
+            viewTreeObserver.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                @Override
+                public void onGlobalLayout() {
+                    circularRevealActivity();
+                    rootLayoutAide.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                }
+            });
+        }
+
         initCommandes();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.backButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+            }
+        });
 
     }
 
@@ -41,5 +74,22 @@ public class Aide extends AppCompatActivity {
         commandes.setAdapter(arrayAdapter);
 
 
+    }
+
+    private void circularRevealActivity() {
+
+        int cx = rootLayoutAide.getWidth() / 2;
+        int cy = rootLayoutAide.getHeight() / 2;
+
+        float finalRadius = Math.max(rootLayoutAide.getWidth(), rootLayoutAide.getHeight());
+
+        // create the animator for this view (the start radius is zero)
+        Animator circularReveal = ViewAnimationUtils.createCircularReveal(rootLayoutAide, cx, cy, 0, finalRadius);
+        circularReveal.setDuration(1000);
+
+        // make the view visible and start the animation
+        System.out.println("TA RACE G RGIZH DEBUG");
+        rootLayoutAide.setVisibility(View.VISIBLE);
+        circularReveal.start();
     }
 }
