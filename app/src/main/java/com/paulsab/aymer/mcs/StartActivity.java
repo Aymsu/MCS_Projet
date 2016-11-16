553abd5cbd323b1435671cb0d9e2b340be362df1
Aymsu <aymeri.piganiol@live.fr>
2016-11-16 14:10:01 +0100

    premier jet interface

--- app/src/main/java/com/paulsab/aymer/mcs/StartActivity.java
+++ app/src/main/java/com/paulsab/aymer/mcs/StartActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 
 public class StartActivity extends AppCompatActivity {
+
+    WebView wv;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -17,14 +20,31 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        wv = (WebView)findViewById(R.id.gifWebView);
+        //wv.loadDataWithBaseURL("file:///android_asset/html/gif.html", null, "text/html", "UTF-8", null);
+        wv.loadUrl("file:///android_asset/html/gif.html");
+
 
     }
 
     public void launchAide(View v){
 
+
         Intent i = new Intent(this, Aide.class);
         startActivity(i);
 
+    }
+
+    public void goRecoVocale(View v){
+
+        Intent i = new Intent(this, RecoVocale.class);
+        startActivity(i);
+
+    }
+
+    public void goTest( View v) {
+        Intent i = new Intent(this, CircularRevealActivity.class);
+        startActivity( i );
     }
 
 }
